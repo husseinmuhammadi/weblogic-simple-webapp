b931d8f0c2fce17739bee1c147f4e43e1b4402eb
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2020-10-05 14:08:45 +0330

    WebLogic Tutorial

--- web/src/main/java/com/javastudio/tutorial/j2ee/web/api/HelloResource.java
+++ web/src/main/java/com/javastudio/tutorial/j2ee/web/api/HelloResource.java
@@ -1,5 +1,8 @@
 package com.javastudio.tutorial.j2ee.web.api;
 
+import org.slf4j.Logger;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -7,10 +10,13 @@ import javax.ws.rs.core.Response;
 @Path("/users")
 public class HelloResource {
 
+    @Inject
+    Logger logger;
+
     @GET
     @Path("/list")
     public Response list() {
-        System.out.println("List of users is going to be populated ...");
+        logger.info("List of users is going to be populated ...");
         return Response.ok().build();
     }
 }
